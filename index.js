1cae0cb0d6067e7474133a2bddf0924779054bae
+ <+>
2023-04-05 18:23:30 +0300

    Correction

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let numSet = new Set();
+  
+  for (let num of array) {
+    if (numSet.has(target - num)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+  
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity of the function is determined by the length of the input array.
+The hasTargetSum function has a linear time complexity O(n)
+
 */
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, target) {
+   variable numSet = the new Set();
+  
+  loop through num of array {
+    if (numSet.has(target - num)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+  
+  return false;
+}
+
 */
 
 /*
   Add written explanation of your solution here
+   Add written explanation of your solution here
+  The function looks for a pair of numbers in the input array that add up to the target number.
+  It does this by keeping track of the numbers it has seen so far in a set.
+  For each number in the array, the function checks if the difference between the target and that number is already in the set.
+  If it is, then there exists a pair of numbers in the array that add up to the target, so the function returns true.
+  If the loop completes without finding a pair of numbers that add up to the target, the function returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
